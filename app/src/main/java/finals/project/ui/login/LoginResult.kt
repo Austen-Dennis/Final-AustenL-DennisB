89dd70af24031ada2a103711e1e0c69188627cf7
AustenLowder2002 <112116586+AustenLowder2002@users.noreply.github.com>
2023-03-15 15:22:40 -0400

    initial commit

---
+++ app/src/main/java/finals/project/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package finals.project.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
