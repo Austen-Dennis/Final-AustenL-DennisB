720aeeb4360917e064ee257852f62b5dcb74dfe7
DennisRBeaver <drbeaver2@bsu.edu>
2023-03-20 09:48:11 -0400

    Adds button call to link home page to main activity

--- app/src/main/java/finals/project/ui/HomeActivity.kt
+++ app/src/main/java/finals/project/ui/HomeActivity.kt
@@ -1,12 +1,21 @@
 package finals.project.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import finals.project.R
+import finals.project.smsPage.MainActivity
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.homepage)
+        val intent = Intent(this, MainActivity::class.java)
+        val button = findViewById<View>(R.id.sms)
+            button.setOnClickListener {
+                startActivity(intent)
+            }
     }
 }
