72f350a110397e66f3f6995aee31bcdabc46b59c
DennisRBeaver <drbeaver2@bsu.edu>
2023-04-03 13:52:08 -0400

    Adds database section in friendprofile

--- app/src/main/java/finals/project/data/FriendProfileActivity.kt
+++ app/src/main/java/finals/project/data/FriendProfileActivity.kt
@@ -1,26 +1,19 @@
 package finals.project.data
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.firebase.ui.auth.data.model.User
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
 import finals.project.R
+
 
 class FriendProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_friend_profile)
         val database = FirebaseDatabase.getInstance()
-        //val myRef = database.getReference("message")
         val myRef = database.getReference("message")
-        myRef.push().setValue("Hello, World!")
+
+        myRef.setValue("Hello, World!")
     }
     companion object {
         fun iscreated(): Any {
--- app/src/main/java/finals/project/data/HomeActivity.kt
+++ app/src/main/java/finals/project/data/HomeActivity.kt
@@ -37,7 +37,7 @@ class HomeActivity : AppCompatActivity() {
 
         val profileButton = findViewById<View>(finals.project.R.id.profile)
         profileButton.setOnClickListener {
-            startActivity(intentPROFILE)
+            startActivity(intentFRIENDPROFILE)
         }
         val smsButton = findViewById<View>(finals.project.R.id.sms)
         smsButton.setOnClickListener {
