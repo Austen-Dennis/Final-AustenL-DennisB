c616ec6789ae4feb099ab12b4a008d16b4df981d
AustenLowder2002 <112116586+AustenLowder2002@users.noreply.github.com>
2023-04-12 22:14:00 -0400

    Update activity_latest_messages.xml

--- app/build/generated/data_binding_base_class_source_out/debug/out/finals/project/databinding/ActivityLatestMessagesBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/finals/project/databinding/ActivityLatestMessagesBinding.java
@@ -6,26 +6,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewbinding.ViewBinding;
-import androidx.viewbinding.ViewBindings;
 import finals.project.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
-import java.lang.String;
 
 public final class ActivityLatestMessagesBinding implements ViewBinding {
   @NonNull
   private final ConstraintLayout rootView;
 
-  @NonNull
-  public final Toolbar toolbar2;
-
-  private ActivityLatestMessagesBinding(@NonNull ConstraintLayout rootView,
-      @NonNull Toolbar toolbar2) {
+  private ActivityLatestMessagesBinding(@NonNull ConstraintLayout rootView) {
     this.rootView = rootView;
-    this.toolbar2 = toolbar2;
   }
 
   @Override
@@ -51,19 +43,10 @@ public final class ActivityLatestMessagesBinding implements ViewBinding {
 
   @NonNull
   public static ActivityLatestMessagesBinding bind(@NonNull View rootView) {
-    // The body of this method is generated in a way you would not otherwise write.
-    // This is done to optimize the compiled bytecode for size and performance.
-    int id;
-    missingId: {
-      id = R.id.toolbar2;
-      Toolbar toolbar2 = ViewBindings.findChildViewById(rootView, id);
-      if (toolbar2 == null) {
-        break missingId;
-      }
-
-      return new ActivityLatestMessagesBinding((ConstraintLayout) rootView, toolbar2);
+    if (rootView == null) {
+      throw new NullPointerException("rootView");
     }
-    String missingId = rootView.getResources().getResourceName(id);
-    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
+
+    return new ActivityLatestMessagesBinding((ConstraintLayout) rootView);
   }
 }
