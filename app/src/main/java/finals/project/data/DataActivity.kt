7737dbe61e6e8d0af9a127d9cef5305314f9ff5d
DennisRBeaver <drbeaver2@bsu.edu>
2023-04-24 19:04:41 -0400

    Changes data initialization in DataActivity

--- app/src/main/java/finals/project/data/DataActivity.kt
+++ app/src/main/java/finals/project/data/DataActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.FirebaseApp
 import com.google.firebase.database.*
 import android.os.Bundle
+import finals.project.smsPage.User
 
 
 class DataActivity : AppCompatActivity() {
@@ -17,53 +18,49 @@ class DataActivity : AppCompatActivity() {
         fun dataBase(uid: String?, name: String?, email: String?) {
             val database = FirebaseDatabase.getInstance()
             val myRef = database.getReference("users/$uid")
-            if (uid != null && myRef.child("Name")==null) {
                 myRef.child("Name").setValue(name)
                 myRef.child("Email").setValue(email)
                 myRef.child("uid").setValue(uid)
-            } else if (uid!=null) {
-                myRef.child("Email").setValue(email)
-                myRef.child("uid").setValue(uid)
+        }
+
+            //verifies that companion object can be reached
+            fun isReachable(): Boolean {
+                return true
             }
-        }
 
-        //verifies that companion object can be reached
-        fun isReachable(): Boolean {
-            return true
-        }
-
-        fun nameChange(uid: String?, name: String?) {
-            val database = FirebaseDatabase.getInstance()
-            val myRef = database.getReference("users/$uid")
-            if (uid != null) {
-                myRef.child("Name").setValue(name)
+            fun nameChange(uid: String?, name: String?) {
+                val database = FirebaseDatabase.getInstance()
+                val myRef = database.getReference("users/$uid")
+                if (uid != null) {
+                    myRef.child("Name").setValue(name)
+                }
             }
-        }
 
-        fun gitHubLink(uid: String?, newLink: String) {
-            val database = FirebaseDatabase.getInstance()
-            val myRef = database.getReference("users/$uid")
-            if (uid != null) {
-                myRef.child("GitHub Link").setValue(newLink)
+            fun gitHubLink(uid: String?, newLink: String) {
+                val database = FirebaseDatabase.getInstance()
+                val myRef = database.getReference("users/$uid")
+                if (uid != null) {
+                    myRef.child("GitHub Link").setValue(newLink)
+                }
             }
-        }
 
-        fun bioAdd(uid: String?, newBio: String) {
-            val database = FirebaseDatabase.getInstance()
-            val myRef = database.getReference("users/$uid")
-            if (uid != null) {
-                myRef.child("Bio").setValue(newBio)
+            fun bioAdd(uid: String?, newBio: String) {
+                val database = FirebaseDatabase.getInstance()
+                val myRef = database.getReference("users/$uid")
+                if (uid != null) {
+                    myRef.child("Bio").setValue(newBio)
+                }
             }
-        }
 
-        fun collegeEmail(uid: String?, collegeEmail: String) {
-            val database = FirebaseDatabase.getInstance()
-            val myRef = database.getReference("users/$uid")
-            if (uid != null) {
-                myRef.child("College Email").setValue(collegeEmail)
+            fun collegeEmail(uid: String?, collegeEmail: String) {
+                val database = FirebaseDatabase.getInstance()
+                val myRef = database.getReference("users/$uid")
+                if (uid != null) {
+                    myRef.child("College Email").setValue(collegeEmail)
+                }
             }
         }
     }
-}
+
 
 
