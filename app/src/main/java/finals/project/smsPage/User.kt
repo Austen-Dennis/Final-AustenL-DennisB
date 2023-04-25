4cd76cd665b82b1d100b5ad5509af5c5f56cb605
bgmeginnis <112116134+bgmeginnis@users.noreply.github.com>
2023-04-25 15:56:34 -0400

    Update User.kt to fix Deprecation

--- app/src/main/java/finals/project/smsPage/User.kt
+++ app/src/main/java/finals/project/smsPage/User.kt
@@ -1,7 +1,7 @@
 package finals.project.smsPage
 
-import kotlinx.android.parcel.Parcelize
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class User(val uid: String, val Name: String, val CollegeEmail: String, val Email: String, val Bio: String, val GitHub: String, val friendRequest: Boolean): Parcelable{
