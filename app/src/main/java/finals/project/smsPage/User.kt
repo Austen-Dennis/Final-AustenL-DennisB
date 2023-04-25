e9c9cf9f4e84086ebb5ba588227988ca53afd1a4
bgmeginnis <112116134+bgmeginnis@users.noreply.github.com>
2023-04-25 15:55:39 -0400

    Update User.kt

--- app/src/main/java/finals/project/smsPage/User.kt
+++ app/src/main/java/finals/project/smsPage/User.kt
@@ -4,6 +4,6 @@ import kotlinx.android.parcel.Parcelize
 import android.os.Parcelable
 
 @Parcelize
-class User(val uid: String, val Name: String, val CollegeEmail: String, val Email: String, val Bio: String, val GitHub: String, val firendrequest: Boolean): Parcelable{
+class User(val uid: String, val Name: String, val CollegeEmail: String, val Email: String, val Bio: String, val GitHub: String, val friendRequest: Boolean): Parcelable{
     constructor(): this("","","","","","", true)
 }
